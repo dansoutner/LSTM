00f504c529de7cddfe5867024222d8e059547476
dansoutner <dan@soutner.cz>
2013-01-25 18:25:17 +0100

    License

--- LSTM.pyx
+++ LSTM.pyx
@@ -5,11 +5,26 @@
 
 """
 Simple implementation of LSTM RNN,
-proposed by Jürgen Schmidhuber, http://www.idsia.ch/~juergen/
+model proposed by Jürgen Schmidhuber, http://www.idsia.ch/~juergen/
 
+Implemented by Daniel Soutner,
+Department of Cybernetics, University of West Bohemia, Plzen, Czech rep.
 dsoutner@kky.zcu.cz, 2013
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
+
 __version__ = "0.3"
 
 import numpy as np
@@ -80,7 +95,7 @@ cdef inline double SigmoidDerivative_f(double x):
 	cdef double act = 1 / (1 + exp(-x))
 	return act * (1 - act)
 
-print "Using Cython module. "
+print "Using Cython module. Version %s" % __version__
 
 
 cdef float oov_log_penalty = -5
@@ -109,7 +124,7 @@ class LSTM:
 		"""
 		cdef float l = 0.
 		self.Reset()
-		oov_log_penalty = -log10(len(self.dic))
+		#oov_log_penalty = -log10(len(self.dic))
 		
 		for ii in range(len(iText)-1):
 			if iText[ii + 1] > -1:
@@ -308,7 +323,7 @@ class LSTM:
 		cdef int len_dic = len(self.dic)
 		p = [self.ppl(iTest)]
 		#p = [10000000]
-		oov_log_penalty = -log10(len(self.dic))
+		#oov_log_penalty = -log10(len(self.dic))
 		
 		cdef int iter = 0
 		while True:
